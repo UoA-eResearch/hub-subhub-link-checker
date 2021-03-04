7a32d227d361495ae82f668dd864e1593f1a83a7
Noel <noel.zeng@auckland.ac.nz>
2021-03-04 13:37:51 +1300

    Add basic functionality

--- src/components/Field.tsx
+++ src/components/Field.tsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { Icon, Paragraph, ValidationMessage } from '@contentful/forma-36-react-components';
-import { CollectionResponse, FieldExtensionSDK, Link } from '@contentful/app-sdk';
-// import '@contentful/forma-36-react-components/dist/styles.css';
+import { CollectionResponse, EntrySys, FieldExtensionSDK, Link } from '@contentful/app-sdk';
 import { CombinedLinkActions, MultipleEntryReferenceEditor } from '@contentful/field-editor-reference';
 import { Entry } from '@contentful/field-editor-reference/dist/types';
+import { LinkActionsProps } from '@contentful/field-editor-reference/dist/components';
+import "./Field.css";
 
 interface FieldProps {
   sdk: FieldExtensionSDK;
@@ -16,15 +17,23 @@ enum CHECKING_STATUS {
   Failed = "FAILED"
 };
 
-function checkPageReferences(sdk: FieldExtensionSDK, subhubId: string, pageId: string): Promise<Boolean> {
+function checkPageReferences(sdk: FieldExtensionSDK, subhubSys: EntrySys, pageSys: EntrySys): Promise<Boolean> {
+  console.log(`Checking page reference for subhub ${subhubSys.id} and page ${pageSys.id}`);
+  // First, check if the content author is trying to link a subhub to itself.
+  if (subhubSys.type === pageSys.type && subhubSys.id === pageSys.id) {
+    console.log("Subhub and page are the same, rejecting.");
+    return Promise.resolve(false);
+  }
   // Fetch other subhubs that have links to this page.
   return sdk.space.getEntries({
     "content_type": "subHub",
-    "fields.internalPages.sys.id": pageId,
-    "sys.id[ne]": subhubId
+    "fields.internalPages.sys.id": pageSys.id,
+    "sys.id[ne]": subhubSys.id
   }).then((entries: CollectionResponse<Object>) => {
-    console.table(entries.items);
-    console.log(`Number of other subhub that contains this page: ${entries.items.length}`);
+    console.log(`Found ${entries.items.length} other Subhub(s) that contain this page.`);
+    if (entries.items.length > 0) {
+      console.log("Other SubHub(s) containing this page",entries.items);
+    }
     return entries.items.length === 0;
   });
 }
@@ -34,31 +43,19 @@ function checkPageReferences(sdk: FieldExtensionSDK, subhubId: string, pageId: s
  * @param sdk The Contentful SDK.
  */
 function checkSubhubPages(sdk: FieldExtensionSDK): Promise<boolean | Array<Link>> {
-  const entryId = sdk.entry.getSys().id;
-  const linkedPages = sdk.entry.fields["internalPages"].getValue() as Array<Link>;
+  const entrySys = sdk.entry.getSys();
+  const linkedPages = sdk.entry.fields["internalPages"].getValue() || [] as Array<Link>;
   if (linkedPages.length === 0) {
     // If there are no linked pages in this SubHub, there aren't any problems.
     return Promise.resolve(true);
   }
-  return Promise.all(linkedPages.map(page => {
-    const pageId = page.sys.id;
-    // Fetch other subhubs that have links to this page.
-    return sdk.space.getEntries({
-      "content_type": "subHub",
-      "fields.internalPages.sys.id": pageId,
-      "sys.id[ne]": entryId
-    }).then((entries: CollectionResponse<Object>) => {
-      console.table(entries.items);
-      console.log(`Number of other subhub that contains this page: ${entries.items.length}`);
-      return entries.items.length === 0 || page;
+  return Promise.all(linkedPages.map((page: Link) => {
+    return sdk.space.getEntry(page.sys.id).then(pageEntry => {
+      // Fetch other subhubs that have links to this page.
+      return checkPageReferences(sdk, entrySys, (pageEntry as Entry).sys);
     });
-    // return sdk.space.getEntry(page.sys.id).then(entry => {
-    //   console.log(`${page.sys.id}`);
-    //   console.table(entry);
-    //   return true;
-    // });
-  })).then((results: Array<true | Link>) => {
-    const failedPages = results.filter(value => value !== true);
+  })).then((results: any []) => {
+    const failedPages = (results as boolean[]).filter(value => !value);
     return failedPages.length === 0;
   });
 }
@@ -80,21 +77,108 @@ const CircularSubhubValidator = ({ sdk }: FieldProps) => {
         }
       });
     }
-    // internalPagesField.onValueChanged(doCheckStatus);
-    // doCheckStatus();
+    const removeValueChangedCb = internalPagesField.onValueChanged((value) => {
+      console.log(`doCheckStatus called with ${value}.`);
+      doCheckStatus();
+    });
+    doCheckStatus();
+    return () => {
+      removeValueChangedCb();
+    }
   }, [sdk, setStatus]);
 
   switch (status) {
     case CHECKING_STATUS.Checking:
       return <Paragraph>Checking...</Paragraph>;
     case CHECKING_STATUS.OK:
-      return <Paragraph><Icon color="positive" icon="CheckCircle" /> No reference issues detected.</Paragraph>;
+      return null;
     case CHECKING_STATUS.Failed:
-      return <ValidationMessage>One of your SubHub pages belongs to another SubHub that links back to the SubHub itself, forming a cycle. Please remove these page(s) from the SubHub. </ValidationMessage>;
+      return <ValidationMessage>Some of your SubHub pages belong(s) to another SubHub. Pages may only belong to one SubHub. Please remove these page(s) from the SubHub. </ValidationMessage>;
     default:
       return <Paragraph>Checking...</Paragraph>;
   }
 }
+
+
+const doFailedAlertDialog = (sdk: FieldExtensionSDK, hasMultiplePages: boolean, failedPageNames : string[]) => {
+  const page = hasMultiplePages ? "pages" : "page";
+  const failedPage = failedPageNames.length > 1 ? "these pages" : "this page";
+  const failedPageList = failedPageNames.map(name => "\"" + name + "\"").join(", ");
+  const nextStep = hasMultiplePages ? `Try adding ${failedPage} to the External Pages field instead: ${failedPageList}.` : "Try adding the page to the External Pages field instead."
+  sdk.dialogs.openAlert({
+    title: `Adding ${page} to SubHub Internal Pages field failed`,
+    message: `Failed to add the ${page} you chose to the SubHub. Pages may only belong to one SubHub's Internal Pages field, and SubHubs may not link to themselves. \
+    The External Pages field does not have these limitations. ${nextStep}`
+  });
+};
+
+/**
+ * Determine and return all the content types accepted by the internalPages field.
+ * @param sdk The field SDK
+ */
+const getContentTypesAcceptedByField = (sdk: FieldExtensionSDK) => {
+  if (sdk.field.items && sdk.field.items.validations) {
+    const contentTypeValidations = sdk.field.items.validations.filter(validation => validation.hasOwnProperty("linkContentType")) as Array<any>;
+    return contentTypeValidations.flatMap(validation => validation.linkContentType || []);
+  } else {
+    return []; // By default, all content types should be allowable in this collection.
+  }
+
+};
+
+interface CustomLinkActionsProps {
+  inheritedProps: LinkActionsProps,
+  sdk: FieldExtensionSDK
+};
+
+const CustomLinkActions = ({inheritedProps:props, sdk}: CustomLinkActionsProps) => {
+  const locale = sdk.locales.default;
+  return <CombinedLinkActions
+  {...props}
+  onLinkExisting={index => {
+    // This callback is called when the user wants to "link" existing pages to the subhub's internalPages collection.
+    const contentTypes = getContentTypesAcceptedByField(sdk);
+    sdk.dialogs
+      .selectMultipleEntries({
+        locale: sdk.field.locale,
+        contentTypes
+      })
+      .then((entries) => {
+        if (!entries || entries.length === 0) {
+          return;
+        }
+        // Check all entries are ok.
+        const subhubSys = sdk.entry.getSys();
+        return Promise.all(
+          entries.map(entry => checkPageReferences(sdk, subhubSys, (entry as Entry).sys))
+        ).then(results => {
+          const failedEntries = entries.filter((entry, i) => !results[i]);
+          if (failedEntries.length === 0) {
+            // All OK! Will add all entries to entry list.
+            props.onLinkedExisting(entries as Entry[], index);
+            return;
+          } else {
+            // There were some pages that didn't pass the check. Do not allow them to be entered.
+            const hasMultiplePages = entries.length !== 1;
+            const failedPageNames = failedEntries.map(entry => {
+              const title = (entry as Entry).fields.title;
+              return title ? title[locale] : "(Untitled content)";
+            });
+            doFailedAlertDialog(sdk, hasMultiplePages, failedPageNames);
+            return;
+          }
+        });
+      }).catch(reason => {
+        sdk.dialogs.openAlert({
+          title: "Error occurred in the SubHub link-checking application",
+          message: "Sorry, an error occurred while adding your page(s) to the SubHub Internal Pages field. " +
+            "Please try again. If problems persist, please message the ResearchHub team for assistance."
+        });
+        console.log("Error occurred in SubHub link-checking application", reason);
+      });
+  }}
+/>
+};
 
 const Field = ({ sdk }: FieldProps) => {
   // If you only want to extend Contentful's default editing experience
@@ -102,8 +186,22 @@ const Field = ({ sdk }: FieldProps) => {
   // -> https://www.contentful.com/developers/docs/extensibility/field-editors/
   // return <Paragraph>Hello Entry Field Component</Paragraph>;
   useEffect(() => {
+    const subhubPages = sdk.field.getValue();
+    console.log(subhubPages);
+    if (!subhubPages || subhubPages.length < 3) {
+          // Need to check in order to fix height problem;
+          console.log("Setting height manually");
+          sdk.window.updateHeight(700);
+    }
     sdk.window.startAutoResizer();
-  }, [sdk]);
+    console.log("Size is ", Math.ceil(document.documentElement.getBoundingClientRect().height));
+    window.addEventListener("resize", () => {
+      console.log("Size changed! New size is ", Math.ceil(document.documentElement.getBoundingClientRect().height));
+    })
+    return () => {
+      sdk.window.stopAutoResizer();
+    }
+  }, [sdk.window, sdk.field]);
   return <div>
     <MultipleEntryReferenceEditor
       viewType="link"
@@ -117,52 +215,10 @@ const Field = ({ sdk }: FieldProps) => {
         },
       }}
       renderCustomActions={
-        props => {
-          return <CombinedLinkActions
-            {...props}
-
-            onLinkExisting={index => {
-              let contentTypes = []; // By default, all content types should be allowable in this collection.
-              if (sdk.field.items && sdk.field.items.validations) {
-                const contentTypeValidations = sdk.field.items.validations.filter(validation => validation.hasOwnProperty("linkContentType")) as Array<any>;
-                contentTypes = contentTypeValidations.flatMap(validation => validation.linkContentType);
-              }
-              console.log(contentTypes);
-              sdk.dialogs
-                .selectMultipleEntries({
-                  locale: sdk.field.locale,
-                  contentTypes
-                })
-                .then((entries) => {
-                  if (!entries || entries.length === 0) {
-                    return;
-                  }
-                  // Check all entries are ok.
-                  const subhubId = sdk.entry.getSys().id;
-                  Promise.all(
-                    entries.map(entry => checkPageReferences(sdk, subhubId, (entry as Entry).sys.id))
-                  ).then(results => {
-                    const failedEntries = entries.filter((entry, i) => !results[i]);
-                    const okEntries = entries.filter((entry, i) => results[i]);
-                    if (failedEntries.length === 0) {
-                      // All OK! Will add all entries to entry list.
-                      props.onLinkedExisting(entries as Entry[], index);
-                      return;
-                    } else {
-                      sdk.dialogs.openAlert({
-                        title: "Adding pages failed",
-                        message: "Failed to add your pages. One of the pages you chose belongs to another SubHub that links back to the SubHub itself, forming a cycle."
-                      });
-                      return;
-                    }
-                  });
-                });
-            }}
-          />
-        }
+        props => <CustomLinkActions inheritedProps={props} sdk={sdk}/>
       }
     />
-    <CircularSubhubValidator sdk={sdk} />
+    {/* <CircularSubhubValidator sdk={sdk} /> */}
   </div>;
 
 };
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,13 +1,79 @@
-import React from 'react';
-import { Paragraph } from '@contentful/forma-36-react-components';
-import { SidebarExtensionSDK } from '@contentful/app-sdk';
+import React, { useEffect, useState } from 'react';
+import { Paragraph, Icon } from '@contentful/forma-36-react-components';
+import { CollectionResponse, EntryAPI, EntrySys, Link, SidebarExtensionSDK } from '@contentful/app-sdk';
 
+enum CHECKING_STATUS {
+  Initial = "INITIAL",
+  Checking = "CHECKING",
+  OK = "OK",
+  Failed = "FAILED"
+};
 interface SidebarProps {
   sdk: SidebarExtensionSDK;
 }
 
-const Sidebar = (props: SidebarProps) => {
-  return <Paragraph>Hello Sidebar Component</Paragraph>;
+/**
+ * Checks and returns if 
+ * @param sdk The Contentful SDK.
+ */
+function checkSubhubPages(sdk: SidebarExtensionSDK) : Promise<boolean | Array<Link>> {
+  const entryId = sdk.entry.getSys().id;
+  const linkedPages = sdk.entry.fields["internalPages"].getValue() as Array<Link>;
+  if (linkedPages.length === 0) {
+    // If there are no linked pages in this SubHub, there aren't any problems.
+    return Promise.resolve(true);
+  }
+  return Promise.all(linkedPages.map(page => {
+    const pageId = page.sys.id;
+    // Fetch other subhubs that have links to this page.
+    return sdk.space.getEntries({
+      "content_type": "subHub",
+      "fields.internalPages.sys.id": pageId,
+      "sys.id[ne]":entryId
+    }).then((entries: CollectionResponse<Object>) => {
+      console.table(entries.items);
+      console.log(`Number of other subhub that contains this page: ${entries.items.length}`);
+      return entries.items.length === 0 || page;
+    });
+    // return sdk.space.getEntry(page.sys.id).then(entry => {
+    //   console.log(`${page.sys.id}`);
+    //   console.table(entry);
+    //   return true;
+    // });
+  })).then((results : Array<true | Link>) => {
+    const failedPages = results.filter(value => value !== true);
+    return failedPages.length === 0;
+  });
+}
+
+const Sidebar = ({sdk}: SidebarProps) => {
+  const [status, setStatus] = useState(CHECKING_STATUS.Checking);
+  useEffect(() => {
+    const internalPagesField = sdk.entry.fields["internalPages"];
+    function doCheckStatus(){
+      setStatus(CHECKING_STATUS.Checking);
+      checkSubhubPages(sdk).then(result => {
+        if (result === true) {
+          setStatus(CHECKING_STATUS.OK);
+        } else {
+          setStatus(CHECKING_STATUS.Failed);
+        }
+      });
+    }
+    internalPagesField.onValueChanged(doCheckStatus);
+    doCheckStatus();
+  }, [sdk, setStatus]);
+
+  switch (status){
+    case CHECKING_STATUS.Checking:
+      return <Paragraph>Checking...</Paragraph>;
+    case CHECKING_STATUS.OK:
+      return <Paragraph><Icon color="positive" icon="CheckCircle" /> No reference issues detected.</Paragraph>;
+    case CHECKING_STATUS.Failed:
+      return <Paragraph><Icon color="negative" icon="ErrorCircle" /> One of your SubHub pages belongs to another SubHub that links back to the SubHub itself, forming a cycle. Please remove these page(s) from the SubHub.</Paragraph>;
+    default:
+      return <Paragraph>Checking...</Paragraph>;
+  }
 };
 
 export default Sidebar;
